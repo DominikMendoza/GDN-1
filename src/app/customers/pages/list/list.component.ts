5cf0e9b76086bf8ba7e035d5fd9544f8b5764ee1
dominikmendoza <u20201b980@upc.edu.pe>
2023-06-10 10:34:11 -0500

    Pages for list and filter

--- src/app/customers/pages/list/list.component.ts
+++ src/app/customers/pages/list/list.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Customer } from '../../models/customer';
+import { CustomerService } from '../../services/customer.service';
+import * as _ from 'lodash';
+
 
 @Component({
   selector: 'app-list',
   templateUrl: './list.component.html',
   styleUrls: ['./list.component.css']
 })
-export class ListComponent {
+export class ListComponent implements OnInit{
+  customers: Customer[] = [];
+  
+  constructor(private customerService: CustomerService) { }
+  ngOnInit(): void {
+    this.getCustomers();
+  }
 
+  getCustomers(): void {
+    this.customerService.getCustomers().subscribe(customers => this.customers = customers);
+  }
 }
